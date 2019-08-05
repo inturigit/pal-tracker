2c230c12e640ec5b346b61d7db1f8fbc37fa01c6
Venkat Inturi <inturi.mi@gmail.com>
2019-08-05 15:26:18 -0400

    config Lab

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class WelcomeController {
-    @GetMapping("/")
-    public String sayHello() {
-        return "hello";
+
+    private String message=null;
+
+    //@Value("${welcome.message}")
+
+    public WelcomeController(@Value("${welcome.message}") String message){
+        this.message=message;
     }
 
+    @GetMapping("/")
+    public String sayHello() {
+        return this.message;
+    }
 }
